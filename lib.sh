4e8f21d54dbc97f0ab47df4427462cbb16af9d3c
carlos coelho <carloscoelho_@msn.com>
2018-06-11 15:57:45 -0300

    Update lib.sh

--- lib.sh
+++ lib.sh
@@ -45,9 +45,6 @@ Install_Apache() {
 Install_php() {
     echo " -------------- Installing PHP -------------- " 
     cd $DOWNDIR
-    sudo apt-get install python-software-properties || local ERROR=1
-    sudo add-apt-repository ppa:ondrej/php || local ERROR=1
-    sudo apt-get update || local ERROR=1
     sudo aapt-get -y install php7.2 libapache2-mod-php7.2 || local ERROR=1
     sudo apt-get install php7.2-mysql php7.2-curl php7.2-json php7.2-cgi php7.2-xsl || local ERROR=1
     sudo apt-get -y install php7.2-mysql php7.2-curl php7.2-gd php7.2-intl php-pear php-imagick php7.2-imap php-memcache  php7.2-pspell php7.2-recode php7.2-sqlite3 php7.2-tidy php7.2-xmlrpc php7.2-xsl php7.2-mbstring php-gettext || local ERROR=1
@@ -57,6 +54,7 @@ Install_php() {
     sudo apt-get -y install php7.2-opcache php-apcu || local ERROR=1
     sudo systemctl restart apache2 || local ERROR=1
     sudo a2enmod ssl || local ERROR=1
+    sudo systemctl restart apache2 || local ERROR=1
     sudo a2ensite default-ssl || local ERROR=1
     sudo systemctl restart apache2 || local ERROR=1
     return $ERROR
